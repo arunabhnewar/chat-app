1a02121a0307f0e5ac1d55a7a7306e323e1c7c52
arunabhnewar <newardevil@gmail.com>
2023-05-31 18:09:34 +0600

    login func setup in login controller

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,8 +1,77 @@
+// external imports
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+
+
+// internal imports
+const User = require('../models/People');
+
+
+
 // get login page
 function getLogin(req, res, next) {
     res.render("pages/index")
 };
 
 
+// do login
+async function login(req, res, next) {
+    try {
+        // find a user who has this email/mobile number
+        const user = await User.findOne({
+            $or: [{ email: req.body.username }, { mobile: req.body.username }]
+        });
+
+        if (user && user._id) {
+            const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+
+            if (isValidPassword) {
+                // prepare the user object to generate token
+                const userObject = {
+                    username: user.username,
+                    email: user.email,
+                    mobile: user.mobile,
+                    role: "user",
+                };
+
+                // generate token
+                const token = jwt.sign(userObject, process.env.JWT_SECRET, {
+                    expiresIn: process.env.JWT_EXPIRE,
+                });
+
+                // set cookie
+                res.cookie(process.env.COOKIE_NAME, token, {
+                    maxAge: process.env.JWT_EXPIRE,
+                    httpOnly: true,
+                    signed: true,
+                });
+
+                // set logged in user local identifier
+                res.locals.loggedInUser = userObject;
+
+                res.render("pages/inbox")
+
+            } else {
+                throw createError("Login failed! Please try again.");
+            }
+
+        } else {
+            throw createError("Login failed! Please try again.");
+        }
+
+    } catch (err) {
+        res.render("pages/index", {
+            errors: {
+                common: {
+                    msg: err.message,
+                },
+            },
+        });
+    };
+};
+
+
+
 // module exports
-module.exports = { getLogin };
+module.exports = { getLogin, login };
