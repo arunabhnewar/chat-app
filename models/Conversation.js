5e9c92916ca037aef060951fcc1aab7800196501
arunabhnewar <newardevil@gmail.com>
2023-06-03 11:48:09 +0600

    conversation, message model & addConversationModal partials file create & script added

---
+++ models/Conversation.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const conversationSchema = mongoose.Schema({
+    creator: {
+        id: mongoose.Types.ObjectId,
+        name: String,
+        avatar: String,
+    },
+    participant: {
+        id: mongoose.Types.ObjectId,
+        name: String,
+        avatar: String,
+    },
+    last_updated: {
+        type: Date,
+        default: Date.now,
+    },
+}, { timestamps: true }
+);
+
+
+const Conversation = mongoose.model('Conversation', conversationSchema);
+
+// module exports
+module.exports = Conversation;
