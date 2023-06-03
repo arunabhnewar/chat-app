14fb3eb435932be76a6a2c2fc0d11df6541ac4ed
arunabhnewar <newardevil@gmail.com>
2023-06-03 12:26:26 +0600

    getInbox func in inboxController & inbox.ejs file corrected

--- controller/inboxController.js
+++ controller/inboxController.js
@@ -1,7 +1,29 @@
+// external imports
+const createError = require("http-errors");
+
+
+
+// internal imports
+const Conversation = require("../models/Conversation");
+
+
+
 // get inbox page
-function getInbox(req, res, next) {
-    res.render("pages/inbox");
+async function getInbox(req, res, next) {
+    try {
+        const conversations = await Conversation.find({
+            $or: [{ "creator.id": req.user.userid }, { "participant.id": req.user.userid }]
+        });
+
+        res.locals.data = conversations;
+        res.render("pages/inbox");
+    } catch (err) {
+        next(err);
+    }
 };
+
+
+// search user
 
 
 // module exports
